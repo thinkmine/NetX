294fd202f910d3640099ae22a4afa60e768ceb7a
Edward Moemeka <edward.moemeka@thinkmine.com>
2025-09-12 05:22:50 -0400

    update to script

--- build_script.cs
+++ build_script.cs
@@ -4,7 +4,11 @@ using static netx;
 
 print("Script customization");
 
+//check the current directory
 pwd();
 
 //copy the created nuget package to the root folder
 copy("NetXFramework/bin/release/thinkmine.netx.framework.1.0.5-preview.nupkg", ".");
+
+//see what was added to the folder
+list();
